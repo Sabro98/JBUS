78848c5a501c714852aabaf1850c9031adc43eb0
Sabro98 <inhyuk04@gmail.com>
2021-09-15 03:01:42 +0900

    docs: comment to ForPlayerSpawn

--- Assets/Scripts/PlayerScript/ForPlayerSpawn.cs
+++ Assets/Scripts/PlayerScript/ForPlayerSpawn.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+//Room에 접속할 때 접속하는 대상인 Player 정보를 저장하기 위함
 public class ForPlayerSpawn : MonoBehaviour
 {
     public JBUS_Player SpawnPlayer { get; set; }
